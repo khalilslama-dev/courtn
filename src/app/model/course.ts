b6777b7990c8d74e925075d34ef23e5e836b52db
khalilslama-dev <ahmedkhalil.slama@esprit.tn>
2020-12-12 23:33:12 +0100

    added model and service

---
+++ src/app/model/course.ts
@@ -0,0 +1,7 @@
+export class Course {
+  constructor(public title: string, public description: string,public image: string){
+    this.title = title;
+    this.description = description;
+    this.image = image;
+  }
+}
